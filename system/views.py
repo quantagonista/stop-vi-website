2a90b21160bf25b717e466967df1c38d1f65107a
quantagonista <quantagonista@quantagonistas-iMac.local>
2018-03-02 21:26:28 +0600

    added loco???

--- system/views.py
+++ system/views.py
@@ -4,8 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import translation
+
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'system/index.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        user_language = 'en'
+        translation.activate(user_language)
+        return render(request, self.template_name)
